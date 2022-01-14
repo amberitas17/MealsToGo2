fcbbd94e3dac8b1f8306c14cce7996a6c94ac8ed
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-14 11:02:15 +0800

    firebase but it was not implementing functions due to payment

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,6 +1,12 @@
 import camelize from "camelize";
 
-export const locationRequest = (searchTerm) => {};
+export const locationRequest = (searchTerm) => {
+  return fetch(
+    `http://5a8f-122-2-97-13.ngrok.io/mealstogo-d9b2e/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
+  });
+};
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
