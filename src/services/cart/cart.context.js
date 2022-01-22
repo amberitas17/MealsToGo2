0696491aad6ee6e554272aa757005f87521b2d7e
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-22 10:55:01 +0800

    improving the cart and the checkout

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -6,6 +6,18 @@ export const CartContextProvider = ({ children }) => {
   const { user } = useContext(AuthenticationContext);
   const [cart, setCart] = useState([]);
   const [restaurant, setRestaurant] = useState(null);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    if (!cart.length) {
+      setSum(0);
+      return;
+    }
+    const newSum = cart.reduce((acc, { price }) => {
+      return (acc += price);
+    }, 0);
+    setSum(newSum);
+  }, [cart]);
   const add = (item, rst) => {
     if (!restaurant || restaurant.placeId !== rst.placeId) {
       setRestaurant(rst);
@@ -27,6 +39,7 @@ export const CartContextProvider = ({ children }) => {
         clearCart: clear,
         restaurant,
         cart,
+        sum,
       }}
     >
       {children}
