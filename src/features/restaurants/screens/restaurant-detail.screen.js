81b95441a26de74b90c78e2b965a5cfd043c743f
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-23 18:53:58 +0800

    improving style in restaurant cards

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { List } from "react-native-paper";
+import { List, Divider } from "react-native-paper";
 import { ScrollView } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -21,9 +21,10 @@ export const RestaurantDetailScreen = ({ route }) => {
           onPress={() => setBreakfastExpanded(!breakfastExpanded)}
         >
           <List.Item title="Eggs Benedict" />
+          <Divider />
           <List.Item title="Classic Breakfast" />
         </List.Accordion>
-
+        <Divider />
         <List.Accordion
           title="Lunch"
           left={(props) => <List.Icon {...props} icon="hamburger" />}
@@ -31,9 +32,13 @@ export const RestaurantDetailScreen = ({ route }) => {
           onPress={() => setLunchExpanded(!lunchExpanded)}
         >
           <List.Item title="Burger w/ Fries" />
+          <Divider />
           <List.Item title="Steak Sandwich" />
+          <Divider />
           <List.Item title="Mushroom Soup" />
+          <Divider />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Dinner"
           left={(props) => <List.Icon {...props} icon="food-variant" />}
@@ -41,9 +46,12 @@ export const RestaurantDetailScreen = ({ route }) => {
           onPress={() => setDinnerExpanded(!dinnerExpanded)}
         >
           <List.Item title="Spaghetti Bolognese" />
+          <Divider />
           <List.Item title="Veal Cutlet with Chicken Mushroom Rotini" />
+          <Divider />
           <List.Item title="Steak Frites" />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Drinks"
           left={(props) => <List.Icon {...props} icon="cup" />}
@@ -51,9 +59,13 @@ export const RestaurantDetailScreen = ({ route }) => {
           onPress={() => setDrinksExpanded(!drinksExpanded)}
         >
           <List.Item title="Coffee" />
+          <Divider />
           <List.Item title="Tea" />
+          <Divider />
           <List.Item title="Modelo" />
+          <Divider />
           <List.Item title="Coke" />
+          <Divider />
           <List.Item title="Fanta" />
         </List.Accordion>
       </ScrollView>
--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -8,8 +8,20 @@ import { colors } from "../../../infrastructure/theme/colors";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
+const TransparentSafeArea = styled(SafeArea)`
+  background-color: transparent;
+`;
+const SettingsBackground = styled.ImageBackground.attrs({
+  source: require("../../../../assets/home_bg.jpg"),
+})`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+`;
+
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
+  background-color: rgba(255, 255, 255, 0.4);
 `;
 
 const AvatarContainer = styled.View`
@@ -19,32 +31,57 @@ const AvatarContainer = styled.View`
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
   return (
-    <SafeArea>
-      <AvatarContainer>
-        <Avatar.Icon
-          size={180}
-          icon="human"
-          backgroundColor={colors.brand.primary}
-        />
-        <Spacer position="top" size="large">
-          <Text variant="label">{user.email}</Text>
-        </Spacer>
-      </AvatarContainer>
-      <List.Section>
-        <SettingsItem
-          style={{ padding: 16 }}
-          title="Favourites"
-          description="View your favourites"
-          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
-          onPress={() => navigation.navigate("Favourites")}
-        />
-        <SettingsItem
-          style={{ padding: 16 }}
-          title="Logout"
-          left={(props) => <List.Icon {...props} color="black" icon="door" />}
-          onPress={onLogout}
-        />
-      </List.Section>
-    </SafeArea>
+    <SettingsBackground>
+      <TransparentSafeArea>
+        <AvatarContainer>
+          <Avatar.Icon
+            size={180}
+            icon="human"
+            backgroundColor={colors.brand.primary}
+          />
+          <Spacer position="top" size="large">
+            <Text variant="label">{user.email}</Text>
+          </Spacer>
+        </AvatarContainer>
+        <List.Section>
+          <SettingsItem
+            title="Favourites"
+            description="View your favourites"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.error} icon="heart" />
+            )}
+            onPress={() => navigation.navigate("Favourites")}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Payment"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.secondary} icon="cart" />
+            )}
+            onPress={() => null}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Past Orders"
+            left={(props) => (
+              <List.Icon
+                {...props}
+                color={colors.ui.secondary}
+                icon="history"
+              />
+            )}
+            onPress={() => null}
+          />
+          <Spacer />
+          <SettingsItem
+            title="Logout"
+            left={(props) => (
+              <List.Icon {...props} color={colors.ui.secondary} icon="door" />
+            )}
+            onPress={onLogout}
+          />
+        </List.Section>
+      </TransparentSafeArea>
+    </SettingsBackground>
   );
 };
