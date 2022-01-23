6bcb48101c7e46a4f56da9de0dff09758648eddc
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-01-23 18:23:28 +0800

    polishing the app

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -3,6 +3,7 @@ import { List, Avatar } from "react-native-paper";
 import styled from "styled-components/native";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
+import { colors } from "../../../infrastructure/theme/colors";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
@@ -20,7 +21,11 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+      <Avatar.Icon
+          size={180}
+          icon="human"
+          backgroundColor={colors.brand.primary}
+        />
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
